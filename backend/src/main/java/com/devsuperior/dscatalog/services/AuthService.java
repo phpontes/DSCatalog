372cc827cf36ae0d8137c35a823d3a83a563f888
phpontes <ppontes95@hotmail.com>
2024-11-25 13:39:21 -0300

    Primeiro passo da recuperação de senha

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog.services;
+
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.dto.EmailDTO;
+
+@Service
+public class AuthService {
+
+	public void createRecoverToken(EmailDTO body) {
+		
+	}
+}
