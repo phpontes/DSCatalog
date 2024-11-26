c148c5825d0af0d5cf6a1f759ec45a8f0d1d9fa4
phpontes <ppontes95@hotmail.com>
2024-11-26 15:44:21 -0300

    Salvando uma nova senha caso o token seja válido

--- backend/src/main/java/com/devsuperior/dscatalog/resources/AuthResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/AuthResource.java
@@ -3,11 +3,13 @@ package com.devsuperior.dscatalog.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.dto.NewPasswordDTO;
 import com.devsuperior.dscatalog.services.AuthService;
 
 import jakarta.validation.Valid;
@@ -22,6 +24,12 @@ public class AuthResource {
 	@PostMapping(value = "/recover-token")
 	public ResponseEntity<Void> createRecoverToken(@Valid @RequestBody EmailDTO body) {
 		authService.createRecoverToken(body);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/new-password")
+	public ResponseEntity<Void> saveNewPassword(@Valid @RequestBody NewPasswordDTO body) {
+		authService.saveNewPassword(body);
 		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -1,14 +1,17 @@
 package com.devsuperior.dscatalog.services;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.dto.NewPasswordDTO;
 import com.devsuperior.dscatalog.entities.PasswordRecover;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.PasswordRecoverRepository;
@@ -23,6 +26,9 @@ public class AuthService {
 	
 	@Value("${email.password-recover.uri}")
 	private String recoverUri;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -53,5 +59,17 @@ public class AuthService {
 				+ recoverUri + token + ". Link válido por " + tokenMinutes + " minutos."; 
 		
 		emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
+	}
+
+	@Transactional
+	public void saveNewPassword(NewPasswordDTO body) {
+		List<PasswordRecover> result = passwordRecoverRepository.searchValidTokens(body.getToken(), Instant.now());
+		if (result.size() == 0) {
+			throw new ResourceNotFoundException("Token inválido");
+		}
+		
+		User user = userRepository.findByEmail(result.get(0).getEmail());
+		user.setPassword(passwordEncoder.encode(body.getPassword()));
+		user = userRepository.save(user);
 	}
 }
