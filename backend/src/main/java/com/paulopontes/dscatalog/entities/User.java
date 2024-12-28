c2c825db79236066f7b02dd8c937ffdfc032a01f
phpontes <ppontes95@hotmail.com>
2024-12-28 15:39:54 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/paulopontes/dscatalog/entities/User.java
+++ backend/src/main/java/com/paulopontes/dscatalog/entities/User.java
@@ -5,15 +5,32 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
