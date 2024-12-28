a351007d20b7d9393ae482e6afede325fae01988
phpontes <ppontes95@hotmail.com>
2024-12-28 13:31:03 -0300

    User and Role

--- backend/src/main/java/com/paulopontes/dscatalog/entities/Category.java
+++ backend/src/main/java/com/paulopontes/dscatalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.paulopontes.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -28,6 +31,9 @@ public class Category implements Serializable{
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
@@ -69,6 +75,10 @@ public class Category implements Serializable{
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
